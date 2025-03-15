d361ab9f6229888bfefbbd33d230962d0f7863f1
Affan Khan <mohdaffankhan@users.noreply.github.com>
2025-03-15 14:37:00 +0530

    change in multer middleware

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,16 +1,10 @@
-import path from "node:path";
 import express from "express";
 import { registerBook } from "./bookController";
-import multer from "multer";
+import {upload} from "../config/multer";
 import { authenticate } from "../middlewares/authenticate";
 
 // router
 const bookRouter = express.Router();
-
-const upload = multer({
-    dest: path.resolve(__dirname, "../../src/public/uploads"),
-    limits:{ fileSize:  1.05e7 } // 10MB
-})
 
 // routes
 bookRouter.post("/register",
